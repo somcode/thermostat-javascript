87db03832f5e4bfcc217de8add129703c27e6ff0
Somayeh <mail.somayeh@gmail.com>
2018-03-22 17:03:12 +0000

    the upper limit when PSM is on and off

--- jasmine-standalone-3/spec/thermostatSpec.js
+++ jasmine-standalone-3/spec/thermostatSpec.js
@@ -33,4 +33,33 @@ describe('Thermostat', function() {
   it('has power saving mode on by default', function() {
      expect(thermostat.isPowerSavingModeOn()).toBe(true);
    });
+
+   it('can switch PSM off', function() {
+     thermostat.switchPowerSavingModeOff();
+     expect(thermostat.isPowerSavingModeOn()).toBe(false);
+   });
+
+   it('can switch PSM back on', function() {
+     thermostat.switchPowerSavingModeOff();
+     expect(thermostat.isPowerSavingModeOn()).toBe(false);
+     thermostat.switchPowerSavingModeOn();
+     expect(thermostat.isPowerSavingModeOn()).toBe(true);
+   });
+
+   describe('when power saving mode is on', function() {
+     it('has a maximum temprature of 25 degrees', function() {
+       thermostat.up(5);
+       expect(thermostat.getCurrentTemprature()).toEqual(25);
+     });
+
+     it('throw an error if temrature is above 25', function() {
+        thermostat.switchPowerSavingModeOn();
+       expect( function(){ thermostat.up(6) }).toThrow ('limit is 25 when PSM is on!!');
+     });
+
+     it('throw an error if temrature is above 32', function() {
+       thermostat.switchPowerSavingModeOff();
+       expect( function(){ thermostat.up(13) }).toThrow ('limit is 32 when PSM is off!!');
+     });
+   });
 });
--- jasmine-standalone-3/src/thermostat.js
+++ jasmine-standalone-3/src/thermostat.js
@@ -2,7 +2,9 @@
 function Thermostat() {
   this.temprature = 20;
   this.MINIMUM_TEMPRATOR = 10;
-  this.powerSavingModeOn = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
+  this.powerSavingMode = true;
 }
 
 Thermostat.prototype.getCurrentTemprature = function() {
@@ -10,7 +12,16 @@ Thermostat.prototype.getCurrentTemprature = function() {
 }
 
 Thermostat.prototype.up = function(number) {
-  this.temprature += number;
+  if(this.temprature + number > this.MAX_LIMIT_PSM_ON && this.powerSavingMode === true) {
+    throw('limit is 25 when PSM is on!!');
+  }
+  else if(this.temprature + number > this.MAX_LIMIT_PSM_OFF && this.powerSavingMode === false) {
+    throw('limit is 32 when PSM is off!!')
+  }
+  else
+  {
+    this.temprature += number;
+  }
 }
 
 Thermostat.prototype.isMINIMUMTEMPRATOR = function() {
@@ -24,8 +35,16 @@ Thermostat.prototype.down = function(number) {
   else {
   this.temprature -= number;
   }
+}
 
 Thermostat.prototype.isPowerSavingModeOn = function() {
-  return this.powerSavingModeOn === true;
+  return this.powerSavingMode === true;
 }
+
+Thermostat.prototype.switchPowerSavingModeOff = function() {
+  this.powerSavingMode = false;
+}
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
 }
