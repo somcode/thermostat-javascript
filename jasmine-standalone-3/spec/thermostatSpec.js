81500d393ba00ac354bf638c2c25026a38ca59b8
Somayeh <mail.somayeh@gmail.com>
2018-03-22 15:22:11 +0000

    throw an error if the temprator is below 10

--- jasmine-standalone-3/spec/thermostatSpec.js
+++ jasmine-standalone-3/spec/thermostatSpec.js
@@ -11,12 +11,22 @@ describe('Thermostat', function() {
   });
 
   it('increases in temprature with up()', function() {
-    thermostat.up();
+    thermostat.up(1);
     expect(thermostat.getCurrentTemprature()).toEqual(21);
   });
 
   it('decreases in temprature with down()', function() {
-    thermostat.down();
+    thermostat.down(1);
     expect(thermostat.getCurrentTemprature()).toEqual(19);
+  });
+
+  it('has a minimum of 10 degree', function() {
+    thermostat.down(10)
+    expect(thermostat.getCurrentTemprature()).toEqual(10);
+  });
+
+  it('throw an error if temrature is below 10', function() {
+
+    expect( function(){ thermostat.down(11) }).toThrow ('is too cold!!');
   });
 });
--- jasmine-standalone-3/src/thermostat.js
+++ jasmine-standalone-3/src/thermostat.js
@@ -1,16 +1,22 @@
 'use strict';
 function Thermostat() {
   this.temprature = 20;
+  this.minimum = 10;
 }
 
 Thermostat.prototype.getCurrentTemprature = function() {
   return this.temprature;
 }
 
-Thermostat.prototype.up = function() {
-  this.temprature += 1;
+Thermostat.prototype.up = function(number) {
+  this.temprature += number;
 }
 
-Thermostat.prototype.down = function() {
-  this.temprature -= 1;
+Thermostat.prototype.down = function(number) {
+  if(this.temprature - number < this.minimum) {
+    throw('is too cold!!');
+  }
+  else {
+  this.temprature -= number;
+  }
 }
