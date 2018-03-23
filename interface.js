581af4be0d4df384502be9b10501f0963ca7d14e
Somayeh <mail.somayeh@gmail.com>
2018-03-23 13:27:37 +0000

    refactoring code for updating thermostat temprature and adding more functionality to buttons

--- interface.js
+++ interface.js
@@ -1,15 +1,43 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temprature').text(thermostat.temprature);
+  updateTemprature();
 
   $('#temprature-up').on('click', function() {
     thermostat.up(1);
-    $('#temprature').text(thermostat.temprature);
+    updateTemprature();
   })
 
   $('#temprature-down').click(function() {
     thermostat.down(1);
-    $('#temprature').text(thermostat.temprature);
+    updateTemprature();
   })
 
-})
+  $('#temprature-reset').click(function() {
+    thermostat.resetTemprature();
+    updateTemprature();
+  })
+
+  $('#powersaving-on').click(function() {
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on');
+    updateTemprature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off');
+    updateTemprature();
+  })
+
+  function updateTemprature() {
+    $('#temprature').text(thermostat.temprature);
+    if(thermostat.energyUsage() === 'low-usage') {
+      $('#temprature').css('color', 'green')
+    } else if(thermostat.energyUsage() === 'medium-usage') {
+      $('#temprature').css('color', 'black')
+    } else {
+      $('#temprature').css('color', 'red')
+    }
+  };
+
+});
