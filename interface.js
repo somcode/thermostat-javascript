b458833234d0bd01b2a8b41db3d19abed61dae56
Somayeh <mail.somayeh@gmail.com>
2018-03-23 17:54:33 +0000

    checking other cities temperature

--- interface.js
+++ interface.js
@@ -34,8 +34,14 @@ $(document).ready(function() {
     $('#temprature').attr('class', thermostat.energyUsage());
   };
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
+
+
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
   })
 
 });
